094de36aab14e7df5d7f0c944799510ff1ac082f
Achala Rajapaksha <achalarajapaksha01@gmail.com>
2024-06-21 22:37:56 +0530

    Cart page corrected

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -6,18 +6,18 @@ function Cart({ cartItems, setCartItems }) {
   let total = 0;
 
   cartItems.forEach((item) => {
-    total += item.price*item.Qty;
+    total += item.price * item.Qty;
   });
 
   return (
     <div className="cart-container">
       <h2>Your order</h2>
       <div className="cart-items">
-        {cartItems.map((item) => (<Cartitem key={item.id} item={item} />)
-
-        )}
+        {cartItems.map((item) => (
+          <Cartitem key={item.id} item={item} />
+        ))}
       </div>
-      <div className="total">Total: {total}</div>
+      <div className="total">Total: Rs. {total}</div>
     </div>
   );
 }
